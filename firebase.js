766fa78a9c00cf7b5bef1681468caead472b2727
joiceestevao <joiceestevao2514@gmail.com>
2025-09-24 22:07:37 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJECT_ID.firebaseapp.com",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_PROJECT_ID.appspot.com",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID"
+};
